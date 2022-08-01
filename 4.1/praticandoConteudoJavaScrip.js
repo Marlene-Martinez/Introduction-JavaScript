2b0c483d895e04c5cb58cb8699a9e0f923abe348
Marlene-Martinez <leles.martinez@gmail.com>
2022-08-01 16:52:29 -0300

    Arquivo de Prática do JavaScript

---
+++ 4.1/praticandoConteudoJavaScrip.js
@@ -0,0 +1,131 @@
+/*const myName = "Carolina"
+const birthCity = "Porto Alegre"
+birthYear = 2030
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);*/
+
+/*let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)*/
+
+/*const base = 5;
+let height = 8;
+
+const area = (base*height)
+console.log(area);*/
+
+/*const perimeter = (5+5+8+8)
+console.log(perimeter)*/
+
+/*const notaDoCandidato = 100;
+if (notaDoCandidato >= 80){
+  console.log("Parabéns você foi aprovado")
+} else if (notaDoCandidato < 80 && notaDoCandidato >=60){
+  console.log("Você está na nossa lista de espera")
+} else if (notaDoCandidato < 60) {
+  console.log("Você foi reprovado")
+}*/
+
+/*const conditionOne = true;
+const conditionTwo = false;
+
+console.log(conditionOne && conditionTwo);*/
+
+/*const currentHour = 5;
+let message = "";
+
+if (currentHour >= 22){
+  console.log(message = "Não deveríamos comer nada, é hora de dormir");
+} else if (currentHour >= 18 && currentHour < 22){
+  console.log(message = "Rango da noite, vamos jantar :D");
+} else if (currentHour >= 14 && currentHour < 18){
+  console.log(message = "Vamos fazer um bolo pro café da tarde?");
+} else if (currentHour >= 11 && currentHour < 14){
+  console.log(message = "Hora do almoço!!!")
+} else if (currentHour >= 4 && currentHour < 11){
+  console.log("Hmmm, cheiro de café recém passado");
+}*/
+
+/*const comida = 'coxinha';
+const bebida = 'cafezinho';
+
+if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}*/
+
+/*const bebidaPrincipal =  'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+}*/
+
+/*console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false*/
+
+/*const weekDay = "sexta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, descanso merecido UwU");
+}*/
+
+/*const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false*/
+
+
+/*mes = 'maio';
+let estacaoDoAno = '?';
+
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        estacaoDoAno = 'Verão';
+        break;
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        estacaoDoAno = 'Outono';
+        break;
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        estacaoDoAno = 'Inverno';
+        break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+        estacaoDoAno = 'Primavera';
+}
+
+console.log(estacaoDoAno); // 'Outono'*/
+    
+/*const number = -5;
+
+if (number > 0) {
+  console.log('positive');
+} else if (number < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+}*/
+
+
+
+
